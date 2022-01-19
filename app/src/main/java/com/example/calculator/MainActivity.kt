75c5273355e39055012d3e59f8c23ffbd47c2e21
Harsh Raj <harsh.raj8833@gmail.com>
2022-01-19 15:21:16 +0530

    onclick listener text display

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,29 +3,52 @@ package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputType
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.textfield.TextInputEditText
+import kotlin.math.exp
+
+
+var newState = true
+var expression = ""
+private const val Tag = "MyActivity"
+
 
 class MainActivity : AppCompatActivity() {
-
-
-    //display.inputType = InputType.TYPE_NULL
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val display: EditText = findViewById(R.id.display)
+        val acButton: FloatingActionButton = findViewById(R.id.ac)
+        acButton.setOnClickListener {
+            expression = ""
+            display.setText("0").toString()
+        }
 
+        val backspaceButton: FloatingActionButton = findViewById(R.id.backspace)
+        backspaceButton.setOnClickListener {
+            if(expression.isNotEmpty()){
+                Log.d(Tag, "in is not empty")
+                expression = expression.dropLast(1)
+                display.setText(expression)
+
+            }
+        }
 
     }
+
+
+
+
+
     fun digitClicked(view: android.view.View) {
         val clickedButton = view as FloatingActionButton
         val display: EditText = findViewById(R.id.display)
-        val acbutton: FloatingActionButton = findViewById(R.id.ac)
         val zeroButton: FloatingActionButton = findViewById(R.id.key0)
         val oneButton: FloatingActionButton = findViewById(R.id.key1)
         val twoButton: FloatingActionButton = findViewById(R.id.key2)
@@ -37,16 +60,42 @@ class MainActivity : AppCompatActivity() {
         val eightButton: FloatingActionButton = findViewById(R.id.key8)
         val nineButton: FloatingActionButton = findViewById(R.id.key9)
         val dotButton: FloatingActionButton = findViewById(R.id.key_decimal)
+
+
         when(clickedButton.id){
-            oneButton.id -> display.setText("1").toString()
-            twoButton.id -> display.setText("2").toString()
-            threeButton.id -> display.setText("3").toString()
-            fourButton.id -> display.setText("4").toString()
-            fiveButton.id -> display.setText("5").toString()
-            sixButton.id -> display.setText("6").toString()
-            sevenButton.id -> display.setText("7").toString()
-            eightButton.id -> display.setText("8").toString()
-            nineButton.id -> display.setText("9").toString()
+
+            zeroButton.id -> {
+                expression += "0"
+            }
+            oneButton.id ->{
+                expression += "1"
+            }
+            twoButton.id -> {
+                expression += "2"
+            }
+            threeButton.id -> {
+                expression += "3"
+            }
+            fourButton.id -> {
+                expression += "4"
+            }
+            fiveButton.id -> {
+                expression += "5"
+            }
+            sixButton.id -> {
+                expression += "6"
+            }
+            sevenButton.id -> {
+                expression += "7"
+            }
+            eightButton.id -> {
+                expression += "8"
+            }
+            nineButton.id -> {
+                expression += "9"
+            }
         }
+        display.setText(expression).toString()
     }
+
 }
